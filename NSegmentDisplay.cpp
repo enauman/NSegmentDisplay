969aa06887f5be7df62aef8815097ce1dd9e3396
enauman <enauman@users.noreply.github.com>
2020-07-02 00:12:33 -0400

    Add files via upload

---
+++ NSegmentDisplay.cpp
@@ -0,0 +1,110 @@
+#include "Arduino.h"
+#include "NSegmentDisplay.h"
+/*
+Create instance of display, argumnets:
+1) common anode or not
+2) number of segment pins
+3) array of segment pins, first 7 in order for number segments if numbers wanted
+*   1
+* 6   2
+*   7
+* 5   3
+*   4 
+4) number of digit pins
+5) array of digit pins, rightmost digit first
+*/
+NSegmentDisplay::NSegmentDisplay(bool common_anode, const int numSegPins, const int * segPins,const int numDigPins, const int * digPins)
+{
+  _common_anode = common_anode;
+  _numSegPins = numSegPins;
+  _segPins = segPins;
+  _numDigPins = numDigPins;
+  _digPins = digPins;
+  for(int i = 0; i<_numSegPins; i++) {
+    pinMode(_segPins[i],OUTPUT);
+  }
+  for(int i = 0; i<_numDigPins; i++) {
+    pinMode(_digPins[i],OUTPUT);
+  }
+}
+
+void NSegmentDisplay::segment(int d, int s, int duration) {
+  off(1);
+  if(_common_anode) {
+    digitalWrite(_digPins[d], HIGH);
+    digitalWrite(_segPins[s], LOW);
+    delay(duration);
+    digitalWrite(_digPins[d], LOW);
+    digitalWrite(_segPins[s], HIGH);
+  } else {
+    digitalWrite(_digPins[d], LOW);
+    digitalWrite(_segPins[s], HIGH);
+    delay(duration);
+    digitalWrite(_digPins[d], HIGH);
+    digitalWrite(_segPins[s], LOW);
+  }
+}
+
+void NSegmentDisplay::blink(int speed) {
+  on(speed);
+  off(speed);
+}
+
+void NSegmentDisplay::number(int d, int num) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(_segPins[i], numbers[num][i]);
+  }
+  for (int j = 0; j < _numDigPins; j++) {
+    if (j == d) {
+      digitalWrite(_digPins[j], LOW);
+    } else {
+      digitalWrite(_digPins[j], HIGH);
+    }
+  }
+  delay(5);
+}
+
+void NSegmentDisplay::on(int duration)
+{
+  for(int j = 0; j<duration / (5 * _numDigPins); j++) {
+    for (int i = 0; i < _numSegPins; i++) {
+      if(_common_anode) {
+        digitalWrite(_segPins[i], LOW);
+      } else {
+        digitalWrite(_segPins[i], HIGH);
+      }
+    }
+    for (int i = 0; i < _numDigPins; i++) {
+      if(_common_anode) {
+        digitalWrite(_digPins[i], HIGH);
+      } else {
+        digitalWrite(_digPins[i], LOW);
+      }
+      delay(5);
+      if(_common_anode) {
+        digitalWrite(_digPins[i], LOW);
+      } else {
+        digitalWrite(_digPins[i], HIGH);
+      }
+    } 
+  } 
+}
+
+void NSegmentDisplay::off(int duration)
+{
+  for (int i = 0; i < _numSegPins; i++) {
+    if(_common_anode) {
+      digitalWrite(_segPins[i], HIGH);
+    } else {
+      digitalWrite(_segPins[i], LOW);
+    }
+  }
+  for (int i = 0; i < _numDigPins; i++) {
+    if(_common_anode) {
+      digitalWrite(_digPins[i], LOW);
+    } else {
+      digitalWrite(_digPins[i], HIGH);
+    }
+  } 
+  delay(duration);
+}
---
+++ NSegmentDisplay.h
@@ -0,0 +1,36 @@
+#ifndef NSegmentDisplay_h
+#define NSegmentDisplay_h
+
+#include "Arduino.h"
+
+class NSegmentDisplay
+{
+
+public:
+	NSegmentDisplay(bool common_anode, const int numSegPins, const int * segPins,const int numDigPins, const int * digPins);
+	void on(int duration);
+	void off(int duration);
+	void blink(int speed);
+	void number(int d, int num);
+	void segment(int d, int s, int duration);
+private:
+	bool _common_anode;
+	int _numSegPins;
+	const int * _segPins;
+	int _numDigPins;
+	const int * _digPins;
+	const int numbers[10][7] = {
+	  {1, 1, 1, 1, 1, 1, 0},//0
+	  {0, 1, 1, 0, 0, 0, 0},//1
+	  {1, 1, 0, 1, 1, 0, 1},//2
+	  {1, 1, 1, 1, 0, 0, 1},//3
+	  {0, 1, 1, 0, 0, 1, 1},//4
+	  {1, 0, 1, 1, 0, 1, 1},//5
+	  {1, 0, 1, 1, 1, 1, 1},//6
+	  {1, 1, 1, 0, 0, 0, 0},//7
+	  {1, 1, 1, 1, 1, 1, 1},//8
+	  {1, 1, 1, 1, 0, 1, 1}//9
+	};
+};
+
+#endif
