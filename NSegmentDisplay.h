5760526e90b587d171848e71c12be0de0f53c15d
Erik Nauman <enauman@hewittschool.org>
2020-07-04 10:25:50 -0400

    Update NSegmentDisplay.h
    *broke out number arrays for common anode and common cathode<br />
    *added overloaded functions for segment

--- NSegmentDisplay.h
+++ NSegmentDisplay.h
@@ -10,8 +10,9 @@ public:
 	NSegmentDisplay(bool common_anode, const int numSegPins, const int * segPins,const int numDigPins, const int * digPins);
 	void on(int duration);
 	void off(int duration);
-	void blink(int speed);
+	void blink(int duration);
 	void number(int d, int num);
+	void segment(int d, int s);
 	void segment(int d, int s, int duration);
 	void crazyEights(int speed);
 private:
@@ -20,7 +21,19 @@ private:
 	const int * _segPins;
 	int _numDigPins;
 	const int * _digPins;
-	const int numbers[10][7] = {
+	const int anodeNumbers[10][7] = {
+	  {0, 0, 0, 0, 0, 0, 1},//0
+	  {1, 0, 0, 1, 1, 1, 1},//1
+	  {0, 0, 1, 0, 0, 1, 0},//2
+	  {0, 0, 0, 0, 1, 1, 0},//3
+	  {1, 0, 0, 1, 1, 0, 0},//4
+	  {0, 1, 0, 0, 1, 0, 0},//5
+	  {0, 1, 0, 0, 0, 0, 0},//6
+	  {0, 0, 0, 1, 1, 1, 1},//7
+	  {0, 0, 0, 0, 0, 0, 0},//8
+	  {0, 0, 0, 0, 1, 0, 0}//9
+	};
+	const int cathodeNumbers[10][7] = {
 	  {1, 1, 1, 1, 1, 1, 0},//0
 	  {0, 1, 1, 0, 0, 0, 0},//1
 	  {1, 1, 0, 1, 1, 0, 1},//2
