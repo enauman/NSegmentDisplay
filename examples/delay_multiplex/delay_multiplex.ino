78dc0781713177f50d64de299248b011579f05f4
Erik Nauman <enauman@hewittschool.org>
2020-07-17 15:35:41 -0400

    Update delay_multiplex.ino

--- examples/delay_multiplex/delay_multiplex.ino
+++ examples/delay_multiplex/delay_multiplex.ino
@@ -5,16 +5,20 @@ const int NUM_DIGIT_PINS = 4;
 const int segments[] = {10, 11, 12, 6, 7, 8, 13, 9};
 const int digits[] = {3, 5, 4, 2};
 NSegmentDisplay disp(false, NUM_SEG_PINS, segments, NUM_DIGIT_PINS, digits);
-int counter = 0;
-int countDelay = 20;
+int state = 0;
+int delayTime = 500;
 double lastChange = 0;
 
 void setup() {}
 
 void loop() {
-  disp.multiDigitNumber(counter);
-  if(millis() > lastChange + countDelay && counter < 10000) {
-    counter++;
+if (state == 0) {
+    disp.multiDigitNumber(96);
+  } else {
+    disp.off(0);
+  }
+  if (millis() > lastChange + delayTime) {
+    state = !state;
     lastChange = millis();
   }
 }
